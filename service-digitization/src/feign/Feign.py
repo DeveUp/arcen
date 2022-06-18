567d44f77806c63159838acedcae3b31e904bb2c
ssbarrio <ssbarrio@bancolombia.com.co>
2022-06-18 07:46:45 -0500

    feat:0000011 - Se normaliza la peticion a otros servicios

--- service-digitization/src/feign/Feign.py
+++ service-digitization/src/feign/Feign.py
@@ -1,54 +1,75 @@
+"""
+    @description - Feign - Comunicacion con otros servicios o microservicios.
+    @version - 1.0.0
+    @creation-date - 2022-06-14
+    @author-creation - Sergio Stives Barrios Buitrago
+    @modification-date - 2022-06-18
+    @author-modification -  Sergio Stives Barrios Buitrago
+"""
 import os
 import requests
 
-from src.util.common import get_http_exception
-from src.util.constant import FEIGN_ENDPOINT
-from src.util.constant import RESPONSE_STATUS_CODE_GET, RESPONSE_STATUS_CODE_POST, RESPONSE_STATUS_CODE_PUT, RESPONSE_STATUS_CODE_DELETE
-from src.util.constant import FEIGN_TYPE_GET, FEIGN_TYPE_POST, FEIGN_TYPE_PUT, FEIGN_TYPE_DELETE
+from src.util.constant import FEIGN
+from src.util.common import get_exception_http
 
 class Feign:
 
     def __init__(self, endpoint):
         self.endpoint = endpoint
+        # Se conoce las respuestas esperada por cada metodo
+        self.operation_get = FEIGN['operation'][0]
+        self.operation_post = FEIGN['operation'][1]
+        self.operation_put = FEIGN['operation'][2]
+        self.operation_delete = FEIGN['operation'][3]
         
     def request(self, endpoint, data):
         endpoint = self.endpoint + endpoint
         return os.environ.get(FEIGN_ENDPOINT) or endpoint
     
-    def response(self, response, code_success= RESPONSE_STATUS_CODE_GET, code_error = None, message_error = None):
-        print(response.status_code)
-        isResponse = True if response.status_code == code_success else False
-        if isResponse == False:
-          raise get_http_exception(code_error, message_error)
+    def response(self, response, code_success, error):
+        try:
+            isResponse = True if response.status_code == code_success else False
+        except:
+            isResponse = False
+        finally:
+            if isResponse == False:
+                raise get_exception_http(error)
         return isResponse
 
-    def send(self, type:int, endpoint, data, code_error = None, message_error = None):
+    def send(self, type:int, endpoint, data, error):  
+        # Se valida el punto de entrada y la informacion
         endpoint = self.request(endpoint, data)
         response = None
-        code_success = RESPONSE_STATUS_CODE_GET
-        if type == FEIGN_TYPE_GET:
-            code_success = RESPONSE_STATUS_CODE_GET
+        # Se consulta el metodo a utilizar la peticion
+        code_success = FEIGN['response']['success']['get']['code']
+        if type == self.operation_get:
+            # Peticion por el metodo GET
+            code_success = FEIGN['response']['success']['get']['code']
             response = requests.get(endpoint, json= data)
-        if type == FEIGN_TYPE_POST:
-            code_success = RESPONSE_STATUS_CODE_POST
+        elif type == self.operation_post:
+            # Peticion por el metodo POST
+            code_success = FEIGN['response']['success']['post']['code']
             response = requests.post(endpoint, json= data)        
-        if type == FEIGN_TYPE_PUT:
-            code_success = RESPONSE_STATUS_CODE_PUT
+        elif type == self.operation_put:
+            # Peticion por el metodo PUT
+            code_success = FEIGN['response']['success']['put']['code']
             response = requests.put(endpoint, json= data)
-        if type == FEIGN_TYPE_DELETE:
-            code_success = RESPONSE_STATUS_CODE_DELETE
+        elif type == self.operation_delete:
+            # Peticion por el metodo DELETE
+            code_success = FEIGN['response']['success']['delete']['code']
             response = requests.delete(endpoint, json= data)
-        self.response(response, code_success, code_error, message_error)
+        # Se valida la respuesta
+        self.response(response, code_success, error)
         return response    
 
-    def get(self, endpoint, data, code_error = None, message_error = None):
-        return self.send(FEIGN_TYPE_GET, endpoint, data, code_error, message_error)    
+    def get(self, endpoint, data, error = None):
+        return self.send(self.operation_get, endpoint, data, error)    
 
-    def post(self, endpoint, data, code_error = None, message_error = None):
-       return self.send(FEIGN_TYPE_POST, endpoint, data, code_error, message_error)    
+    def post(self, endpoint, data, error = None):
+       return self.send(self.operation_post, endpoint, data, error)    
     
-    def put(self, endpoint, data, code_error = None, message_error = None):
-        return self.send(FEIGN_TYPE_PUT, endpoint, data, code_error, message_error)    
+    def put(self, endpoint, data, error = None):
+        return self.send(self.operation_put, endpoint, data, error)    
     
-    def delete(self, endpoint, data, code_error = None, message_error = None):
-        return self.send(FEIGN_TYPE_DELETE, endpoint, data, code_error, message_error)    
+    def delete(self, endpoint, data, error = None):
+        return self.send(self.operation_delete, endpoint, data, error)    
--- service-digitization/src/model/entity/DocumentLocation.py
+++ service-digitization/src/model/entity/DocumentLocation.py
@@ -1,3 +1,11 @@
+"""
+    @description - Entidad ubicacion del documento
+    @version - 1.0.0
+    @creation-date - 2022-06-13
+    @author-creation - Sergio Stives Barrios Buitrago
+    @modification-date - 2022-06-18
+    @author-modification -  Sergio Stives Barrios Buitrago
+"""
 from pydantic import BaseModel
 
 class DocumentLocation(BaseModel):
